673f8ff257e89bf07a03ccc7c3b62719ab2454e2
Corentin POUHET-BRUNERIE <corentin.pouhet-brunerie@smile.fr>
2016-10-05 19:07:45 +0200

    [CLEAN] Remove addons designed for Odoo 10.0

--- smile_ci/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from api import *
-from exceptions import *
-from misc import *
-from osutil import *
--- smile_ci/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import docker_registry_images
